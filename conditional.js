ca100b9dc9f0ad3e2221d65f83beaf3c4cd8794a
ShamimHossain1 <beastb697@gmail.com>
2023-01-28 00:33:25 +0600

    conditional

---
+++ conditional.js
@@ -0,0 +1,26 @@
+var iphone= 75000;
+var budget=95000;
+
+if (iphone < budget)
+{
+    console.log('buy');
+}
+
+if (iphone > budget) 
+{
+    console.log('ditch');
+}
+
+
+var chickenPrice = 500;
+var IHave = 50;
+
+if (chickenPrice > IHave){
+    console.log("I will eat potato");
+
+}
+else{
+    console.log("I will fill my stomach");
+
+}
+
