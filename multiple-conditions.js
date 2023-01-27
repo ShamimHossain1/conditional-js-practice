eb71e06c3d41466077ea83c423adab9a286fecb3
ShamimHossain1 <beastb697@gmail.com>
2023-01-28 00:54:13 +0600

    multiple condition

---
+++ multiple-conditions.js
@@ -0,0 +1,25 @@
+var isgraduated = true;
+var salary = 50000;
+var car = 1;
+var house = false;
+
+if( isgraduated ==  true && salary < 75000 && car >=1 && house == true){
+
+    console.log("esho biye kore feli");    
+}
+else{
+    console.log('Tor kopale biya nai');
+}
+
+var beautiful = true;
+var character = false;
+var money = true;
+
+if ( beautiful == true || character == true || money == false){
+
+    console.log( 'asho prem kori');
+}
+else{
+    console.log('Tmr bandobir number dao');
+}
+
